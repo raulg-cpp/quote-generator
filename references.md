6e460704cb347b8a2cf8910fa7f70a2f3a9327d7
raulg-cpp <raulg.cpp@gmail.com>
2023-05-12 16:10:02 -0400

    initial

---
+++ references.md
@@ -0,0 +1,39 @@
+# References - Bootstrap and FontAwesome in React
+
+## React [Framework]
+
+	https://random-quote-machine.freecodecamp.rocks
+	
+	https://react.dev/learn
+	
+	https://www.educative.io/answers/how-to-add-comments-in-jsx
+	
+## Bootstrap [Css styling]
+
+	https://getbootstrap.com/docs/5.0/components/buttons/
+
+	https://blog.logrocket.com/using-bootstrap-with-react-tutorial-with-examples/#add-bootstrap-react-using-bootstrap-cdn
+	
+	https://react-bootstrap.github.io/getting-started/introduction/
+	
+	https://www.npmjs.com/package/bootstrap?activeTab=versions
+	
+	https://www.youtube.com/watch?v=Tad3gdblZV4
+
+## FontAwesome [Icons]
+
+	https://fontawesome.com/docs/web/use-with/react/
+	
+	https://fontawesome.com/docs/web/use-with/react/add-icons
+	
+	https://fontawesome.com/docs/web/dig-deeper/styles
+	
+	https://dev.to/davidemaye/how-to-set-up-font-awesome-in-react-5a8d
+	
+	https://stackoverflow.com/questions/55877731/how-to-fix-library-is-not-defined-error-in-my-react-app
+	
+	https://stackoverflow.com/questions/71356327/error-caching-was-left-unconfigured-babels-plugins-when-i-installed-font-awes
+	
+	https://babeljs.io/docs/configuration
+	
+	https://fontawesome.com/docs/web/use-with/react/add-icons#add-icons-globally
--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+// font awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+	// global
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+// Individual imports
+/*
+import { faBell } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import {} from '@fortawesome/free-regular-svg-icons'
+*/
+
+// default
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,17 +25,28 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link"
+           href="https://reactjs.org"
+           target="_blank"
+           rel="noopener noreferrer">
           Learn React
         </a>
+       
+       {/* bootstrap */}
+       <button type="button" class="btn btn-primary">Primary</button>        
+       
+       {/* react */}
+       <FontAwesomeIcon icon="fa-brands fa-twitter" />
+       
       </header>
     </div>
   );
 }
 
 export default App;
+library.add(fab, fas, far)
+
+/* // Individual imports
+    <FontAwesomeIcon icon={faBell} />
+    <FontAwesomeIcon icon={faTwitter} />
+*/
